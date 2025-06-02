e68cb39bd08e18b547bd4a2838117621c46353f6
blubspond.adm1 <blubspond.adm1@gmail.com>
2025-06-02 09:28:25 +0000

    correct x and y pos

--- docs/terminal/js/games/reactor-ctrl/reactorCtrlGameSettings.js
+++ docs/terminal/js/games/reactor-ctrl/reactorCtrlGameSettings.js
@@ -1,4 +1,4 @@
-import { parseMapString } from './reactorCtrlGameLogic.js';
+import { handleInventory, parseMapString } from './reactorCtrlGameLogic.js';
 
 // Facility map as a template string
 const facilityMapString = `
@@ -56,6 +56,30 @@ const createLocation = (friendlyName, description, mapMarker, exits, line, colum
   friendlyName, description, mapMarker, exits, line, column,
   securityLevel, hasCreature, hazardLevel
 });
+
+const createPlayer = (hp = 500, ap = 5, currentLocation = "CR", inventory = [], held_r = null, held_l = null, stats = { hunger: 0, insomnia: 0, sanity: 100 }) => ({
+  hp, ap, currentLocation, inventory, held_r, held_l, stats
+});
+
+const createItem = (max_durability = 1000, durability = 1000, is2handed = false, description = "", dph = 1, modifier = []) => ({
+  max_durability, durability, is2handed, description, dph, modifier
+});
+
+const createMonster = {
+  class: "Template",
+  location: null,
+  state: "dormant",
+  isNearPlayer: false,
+  isHostile: true,
+  canNoClip: false,
+  hp: 100,
+  target: null,
+  goal: [],
+  path: []
+}
+
+// === Facility Map ===
+const facilityMap = parseMapString(facilityMapString)
 
 // === Reverse Mapping Utilities ===
 const markerToLocationKey = {
@@ -119,25 +143,25 @@ const getExitsForLocation = locKey =>
 // === Locations ===
 const locationDescriptors = [
   ["ControlRoom", "Control Room", "CR", 27, 45, 3, false, 2], // Added line and column
-  ["ReactorRoom", "Reactor Room", "RR", 5, true, 5, 7, 45],
-  ["TurbineRoom", "Turbine Room", "TR", 4, false, 3, 7, 65],
-  ["GeneratorRoom", "Generator Room", "GR", 3, false, 2, 26, 15],
-  ["ServerRoom", "Server Room", "SR", 2, false, 2, 26, 75],
-  ["CoolantPumpStation", "Coolant Pump Station", "CP", 2, false, 3, 5, 4],
-  ["PowerConverterRoom", "Power Converter Room", "PC", 2, false, 2, 3, 40],
-  ["SpentFuelStorage", "Spent Fuel Storage", "SF", 4, false, 4, 10, 40],
-  ["WaterTreatmentFacility", "Water Treatment Facility", "BW", 3, false, 2, 6, 90],
-  ["ControlArchives", "Control Archives", "CA", 3, false, 1, 17, 95],
-  ["ElectricalSwitchyard", "Electrical Switchyard", "Elect", 2, false, 1, 30, 90],
-  ["Laboratory", "Laboratory", "LAB", 4, false, 3, 26, 4],
-  ["VentilationSystems", "Ventilation Systems", "Vent", 4, false, 2, 15, 4],
-  ["DecontaminationRoom", "Decontamination Room", "DCR", 3, false, 2, 34, 15],
-  ["Bunker", "Bunker", "BU", 5, false, 4, 35, 45],
-  ["HallwayA", "Hallway A", "A", 1, false, 1, 26, 35],
-  ["HallwayB", "Hallway B", "B", 1, false, 1, 26, 55],
-  ["HallwayC", "Hallway C", "C", 1, false, 1, 17, 45],
-  ["HallwayD", "Hallway D", "D", 1, false, 1, 17, 15],
-  ["HallwayE", "Hallway E", "E", 1, false, 1, 17, 75]
+  ["ReactorRoom", "Reactor Room", "RR", 7, 45, 5, true, 5],
+  ["TurbineRoom", "Turbine Room", "TR", 7, 65, 4, false, 3],
+  ["GeneratorRoom", "Generator Room", "GR", 26, 15, 3, false, 2],
+  ["ServerRoom", "Server Room", "SR", 26, 75, 2, false, 2],
+  ["CoolantPumpStation", "Coolant Pump Station", "CP", 5, 4, 2, false, 3],
+  ["PowerConverterRoom", "Power Converter Room", "PC", 3, 40, 2, false, 2],
+  ["SpentFuelStorage", "Spent Fuel Storage", "SF", 10, 40, 4, false, 4],
+  ["WaterTreatmentFacility", "Water Treatment Facility", "BW", 6, 90, 3, false, 2],
+  ["ControlArchives", "Control Archives", "CA", 17, 95, 3, false, 1],
+  ["ElectricalSwitchyard", "Electrical Switchyard", "Elect", 30, 90, 2, false, 1],
+  ["Laboratory", "Laboratory", "LAB", 26, 4, 4, false, 3],
+  ["VentilationSystems", "Ventilation Systems", "Vent", 15, 4, 4, false, 2],
+  ["DecontaminationRoom", "Decontamination Room", "DCR", 34, 15, 3, false, 2],
+  ["Bunker", "Bunker", "BU", 35, 45, 5, false, 4],
+  ["HallwayA", "Hallway A", "A", 26, 35, 1, false, 1],
+  ["HallwayB", "Hallway B", "B", 26, 55, 1, false, 1],
+  ["HallwayC", "Hallway C", "C", 17, 45, 1, false, 1],
+  ["HallwayD", "Hallway D", "D", 17, 15, 1, false, 1],
+  ["HallwayE", "Hallway E", "E", 17, 75, 1, false, 1]
 ];
 
 const locations = Object.fromEntries(
