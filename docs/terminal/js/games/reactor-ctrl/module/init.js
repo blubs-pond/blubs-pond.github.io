710843ee67ca231a1d693159e0ad4dfeb8108fae
ClockWork Production <136562893+MissionLogBlackTiger@users.noreply.github.com>
2025-08-30 07:35:51 +0700

    mod room

--- docs/terminal/js/games/reactor-ctrl/module/init.js
+++ docs/terminal/js/games/reactor-ctrl/module/init.js
@@ -43,29 +43,29 @@ export const map =`
 │             ├───────┤                                             │ │   │ │              │
 │             x <DCR> x                     <B>                     │ │   │ │              │
 │             ├───────┤                                             │ │<4>│ │              │
-└─────────────┘       └─────────────────────────────────────────────┘ └───┘ └──────────────┘`
+└─────────────┘       └─────────────────────────────────────────────┘ └───┘ └──────────────┘`;
 
 export const loc = {
-    CP:     new preset.room("CoolantPumpStation",       "CP",   ["D"],              ),
-    PC:     new preset.room("PowerConverterRoom",       "PC",   ["SF"],             ),
-    SF:     new preset.room("SpentFuelStorage",         "SF",   ["RR", "PC"],       ),
-    RR:     new preset.room("ReactorRoom",              "RR",   ["TR", "SF","C"],   ),
-    TR:     new preset.room("TurbineRoom",              "TR",   ["RR"],             ),
-    BW:     new preset.room("WaterTreatmentFacility",   "BW",   ["E"],              ),
-    CA:     new preset.room("ControlArchives",          "CA",   ["E"],              ),
-    ELE:    new preset.room("ElectricalSwitchyard",     "ELE",  ["E"],              ),
-    SR:     new preset.room("ServerRoom",               "SR",   ["B"],              ),
-    CR:     new preset.room("ControlRoom",              "CR",   ["A", "B", "BU"],   ),
-    GR:     new preset.room("GeneratorRoom",            "GR",   ["A"],              ),
-    LAB:    new preset.room("Laboratory",               "LAB",  ["D", "DCR"],       ),
-    Vent:   new preset.room("VentilationSystems",       "Vent", ["D"],              ),
-    DCR:    new preset.room("DecontaminationRoom",      "DCR",  ["LAB", "BU"],      ),
-    BU:     new preset.room("Bunker",                   "BU",   ["DCR","CR"],       ),
-    A: new preset.hallway("HallwayA",   "A",    ["CR", "GR", "C"],              ),
-    B: new preset.hallway("HallwayB",   "B",    ["CR", "SR", "C"],              ),
-    C: new preset.hallway("HallwayC",   "C",    ["A", "B", "D", "E", "RR"],     ),
-    D: new preset.hallway("HallwayD",   "D",    ["C", "LAB", "Vent", "CP"],     ),
-    E: new preset.hallway("HallwayE",   "E",    ["C", "ELE", "CA", "BU"],       )
+    CP:     new preset.room("CoolantPumpStation",       "CP",   ["D"],              [0,0], [14,10]),
+    PC:     new preset.room("PowerConverterRoom",       "PC",   ["SF"],             [22,9], [30,17]),
+    SF:     new preset.room("SpentFuelStorage",         "SF",   ["RR", "PC"],       [0,0],  [0,0]),
+    RR:     new preset.room("ReactorRoom",              "RR",   ["TR", "SF","C"],   [0,0],  [0,0]),
+    TR:     new preset.room("TurbineRoom",              "TR",   ["RR"],             [0,0],  [0,0]),
+    BW:     new preset.room("WaterTreatmentFacility",   "BW",   ["E"],              [0,0],  [0,0]),
+    CA:     new preset.room("ControlArchives",          "CA",   ["E"],              [0,0],  [0,0]),
+    ELE:    new preset.room("ElectricalSwitchyard",     "ELE",  ["E"],              [0,0],  [0,0]),
+    SR:     new preset.room("ServerRoom",               "SR",   ["B"],              [0,0],  [0,0]),
+    CR:     new preset.room("ControlRoom",              "CR",   ["A", "B", "BU"],   [0,0],  [0,0]),
+    GR:     new preset.room("GeneratorRoom",            "GR",   ["A"],              [0,0],  [0,0]),
+    LAB:    new preset.room("Laboratory",               "LAB",  ["D", "DCR"],       [0,0],  [0,0]),
+    Vent:   new preset.room("VentilationSystems",       "Vent", ["D"],              [0,0],  [0,0]),
+    DCR:    new preset.room("DecontaminationRoom",      "DCR",  ["LAB", "BU"],      [0,0],  [0,0]),
+    BU:     new preset.room("Bunker",                   "BU",   ["DCR","CR"],       [0,0],  [0,0]),
+    A: new preset.hallway("HallwayA",   "A",    ["CR", "GR", "C"],              [0,0],  [0,0]),
+    B: new preset.hallway("HallwayB",   "B",    ["CR", "SR", "C"],              [0,0],  [0,0]),
+    C: new preset.hallway("HallwayC",   "C",    ["A", "B", "D", "E", "RR"],     [0,0],  [0,0]),
+    D: new preset.hallway("HallwayD",   "D",    ["C", "LAB", "Vent", "CP"],     [0,0],  [0,0]),
+    E: new preset.hallway("HallwayE",   "E",    ["C", "ELE", "CA", "BU"],       [0,0],  [0,0])
 };
 
 
